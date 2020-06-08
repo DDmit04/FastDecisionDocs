37af2befa64929225e4c14a25e9f3957fde7576b
Дмитроченков Даниил Андреевич <danya.dmit1@gmail.com>
2020-06-08 13:37:58 +0300

    update docs

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"hackaton.fastdisision"},{"l":"hackaton.fastdisision.config"},{"l":"hackaton.fastdisision.controller"},{"l":"hackaton.fastdisision.data"},{"l":"hackaton.fastdisision.excaptions"},{"l":"hackaton.fastdisision.repo"},{"l":"hackaton.fastdisision.service"},{"l":"hackaton.fastdisision.views"}]
